5dcf80602a0c8ed30f1697b8ad2a1239c2f33857
Hanlaev Muhammed <hanlaevm5@gmail.com>
2026-09-14 10:03:14 +0700

    Initial Commit

---
+++ MobileProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileProject
+//
+//  Created by Мухаммед Ханлаев on 14.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileProject/MobileProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileProjectApp.swift
+//  MobileProject
+//
+//  Created by Мухаммед Ханлаев on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MobileProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
